f9953829cc9cd07e7fdb8bba6122664229ebf516
davidcastilloalvarado <dcastilloa@uni.pe>
2025-12-19 13:42:40 -0500

    lint: some fix

--- broadcast/__init__.py
+++ broadcast/__init__.py
@@ -3,7 +3,7 @@ WebRTC Broadcast Server - Audio & Video
 Broadcasts your PC camera and microphone to multiple viewers using WebRTC
 """
 
-from .app import channels, create_app, socketio
 from . import events
+from .app import channels, create_app, socketio
 
 __all__ = ["create_app", "socketio", "channels", "events"]
